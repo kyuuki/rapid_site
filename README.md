8a69f28f16ec9becf852279d6a21125bfff1613c
kyuuki <kyuuki.japan@gmail.com>
2018-02-06 22:03:29 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# rapid_site
+
+## CSS フレームワーク
+- [Bootstrap 4.0.0](https://getbootstrap.com/)
+- [Bootswatch](https://bootswatch.com/)
+
+## ツール
+- [gulp](https://gulpjs.com/)
+
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var gulp = require('gulp');
+var slim = require('gulp-slim');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('html', function() {
+  gulp.src(['app/**/*.html', 'app/favicon.ico'])
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('slim', function() {
+  gulp.src('app/**/*.slim')
+    .pipe(slim({
+      pretty: true
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('images', function() {
+  gulp.src('app/images/**/*')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.stream());
+});
+    
+gulp.task('styles', function() {
+  gulp.src(['app/styles/**/*.scss', 'app/styles/**/*.css'])
+    .pipe(sass())
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(browserSync.stream());
+});
+    
+gulp.task('scripts', function() {
+  gulp.src('app/scripts/**/*.js')
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(browserSync.stream());
+});
+    
+gulp.task('serve', ['default'], function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  });
+  gulp.watch(['app/**/*.html'], ['html']);
+  gulp.watch(['app/**/*.slim'], ['slim']);
+  gulp.watch(['app/styles/**/*.scss'], ['styles']);
+});
+
+gulp.task('default', ['html', 'slim', 'images', 'styles', 'scripts']);
+
